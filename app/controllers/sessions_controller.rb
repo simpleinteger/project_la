1887538519c5fd20bc4f50a0f36b5b36241b4859
Steven <stevenr2d2@gmail.com>
2013-04-27 20:17:01 -0700

    changed sessions_controller.rb and routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
     :api_method => service.calendar_list.list,
     :parameters => {},
     :headers => {'Content-Type' => 'application/json'})
-
+    
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ HackLa::Application.routes.draw do
   match '/auth/:provider/callback' => 'sessions#create'
 
   resources :users, :only => [:index, :show, :edit, :update ]
-  match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
   match '/auth/failure' => 'sessions#failure'
