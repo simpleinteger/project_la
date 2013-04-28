d903b1f720beab46b59103873d6a1d37918481d9
Steven <stevenr2d2@gmail.com>
2013-04-27 17:05:36 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackLa::Application.config.secret_token = 'd1463474b1b21a5d12dc86463d33b17b07093a76336cc35c0e8c71f57a8db91992458fec997429909758cc8eb319ea41682b2e64d6dd50356ffde72b9c8830c2'
